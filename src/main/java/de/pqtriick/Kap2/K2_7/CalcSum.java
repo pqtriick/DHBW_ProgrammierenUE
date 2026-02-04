646a592050fc8e416ac20e7d90fe2c5dbc76a0d2
Patrick <patickde06@gmail.com>
2026-02-04 12:41:46 +0100

    Hausaufgaben hinzugefügt

--- src/main/java/de/pqtriick/Kap2/K2_7/CalcSum.java
+++ src/main/java/de/pqtriick/Kap2/K2_7/CalcSum.java
@@ -1,3 +1,7 @@
+package de.pqtriick.Kap2.K2_7;
+
+import java.util.Scanner;
+
 /**
  * @author C. Frese
  * DHBW Stuttgart - Vorlesung Programmieren 1
@@ -16,6 +20,44 @@ public class CalcSum {
 				+ useGauss(input));
 		System.out.println("Ergebnis rekursiv: "
 				+ rekursiveSum(input));
+	}
+
+	static int inputInt() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte int-wert angeben:");
+		int num = scanner.nextInt();
+		if (num < 0) {
+			num = Math.abs(num);
+			System.out.println("VZW wurde durchgeführt!");
+		}
+		return num;
+	}
+
+	static int forSum(int num) {
+		int sum = 0;
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+
+	static int useGauss(int num) {
+		int sum = 0;
+		for (int i = 0; i < num; i++) {
+			sum += (int) ((Math.pow(i, 2)+i)/2);
+		}
+		return sum;
+	}
+
+	static int rekursiveSum(int num) {
+		int sum = 0;
+		if (num == 0) {
+			return sum;
+		} else {
+			sum += num;
+			sum += rekursiveSum(num-1);
+			return sum;
+		}
 	}
 
 }
--- src/main/java/de/pqtriick/Kap2/K2_7/TemplateMinMaxRekursiv.java
+++ src/main/java/de/pqtriick/Kap2/K2_7/TemplateMinMaxRekursiv.java
@@ -1,5 +1,8 @@
 package de.pqtriick.Kap2.K2_7;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @author C. Frese DHBW Stuttgart
  * Vorlesung Programmieren1 
@@ -16,6 +19,44 @@ public class TemplateMinMaxRekursiv {
 		printArr(arrInts);
 		System.out.println("Das Maximum lautet: " + maxRek(arrInts, 0));
 		System.out.println("Das Minimum lautet: " + minRek(arrInts, 0));
+	}
+
+	static int[] inputIntArray() {
+		int array[] = new int[50];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * (1000 +1));
+		}
+		return array;
+	}
+
+	static void printArr(int[] array) {
+		System.out.println(Arrays.toString(array));
+	}
+
+	static int maxRek(int[] array, int index) {
+		int max = Integer.MIN_VALUE;
+		if (index == array.length -1) {
+			return max;
+		}
+		int maxOfRest = maxRek(array, index+1);
+		if (array[index] > maxOfRest) {
+			return array[index];
+		} else {
+			return maxOfRest;
+		}
+	}
+
+	static int minRek(int[] array, int index) {
+		int min = Integer.MAX_VALUE;
+		if (index == array.length -1) {
+			return min;
+		}
+		int minOfRest = minRek(array, index+1);
+		if (array[index] > minOfRest) {
+			return minOfRest;
+		} else {
+			return array[index];
+		}
 	}
 
 }
